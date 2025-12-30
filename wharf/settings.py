f0558d0264e6e9aaa0ef7bfebc9ebdd09ecc4c9c
Christian Kaatz <chrkaatz@googlemail.com>
2025-12-30 19:30:33 +0100

    Add context processors for debugging and request handling in TEMPLATES

--- wharf/settings.py
+++ wharf/settings.py
@@ -92,6 +92,8 @@ TEMPLATES = [
         "DIRS": [],
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
